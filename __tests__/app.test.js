506768354c77a45aff727974e764c0fa4c55f669
Jose <j.a.ojeda@live.com>
2020-04-01 19:54:57 -0700

    added test route and app.test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,26 @@ require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
 
-describe('app routes', () => {
-  beforeAll(() => {
-    connect();
+describe('Core App', () => {
+  it('is alive', () => {
+    return request(app)
+      .get('/hello')
+      .expect(200)
+      .then(res => {
+        expect(res.text).toBe('world');
+      });
   });
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
+  it('returns 404 on non-api bad path', () => {
+    return request(app)
+      .get('/bad-path')
+      .expect(404);
   });
 
-  afterAll(() => {
-    return mongoose.connection.close();
+  it('returns application/json 404 on bad api path', () => {
+    return request(app)
+      .post('/api/bad-path')
+      .expect(404);
   });
 });
